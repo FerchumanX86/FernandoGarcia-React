73dc728b88909af4041ab4ce2708d319ef57a07a
FerchumanX86 <122957990+FerchumanX86@users.noreply.github.com>
2023-05-18 05:11:02 -0300

    segundo comit

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <h1 style={{ color: "red", backgroundColor: "yellow", marginTop: "10px" }}>
+        Footer
+      </h1>
+    );
+  };
+  
+  export default Footer;
+  
