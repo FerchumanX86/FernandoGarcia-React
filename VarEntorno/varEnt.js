42638610fe7b71099ef30fde5849eafa06cb31d9
FerchumanX86 <122957990+FerchumanX86@users.noreply.github.com>
2023-07-13 16:47:31 -0300

    vercel.json

--- VarEntorno/varEnt.js
+++ VarEntorno/varEnt.js
@@ -6,3 +6,11 @@
 //   messagingSenderId: "251884467547",
 //   appId: "1:251884467547:web:2838e44cb7bf818678967e",
 // };
+
+
+// VITE_API_KEY=AIzaSyC9gaCv2aa83SViJMWNwu6eDQcB4RvZ5uU
+// VITE_AUTH_DOMAIN=mercado-pavo.firebaseapp.com
+// VITE_PROJECT_ID=mercado-pavo
+// VITE_STORAGE_BUCKET=mercado-pavo.appspot.com
+// VITE_MESSAGING_SENDER_ID=251884467547
+// VITE_APP_ID=1:251884467547:web:2838e44cb7bf818678967e
