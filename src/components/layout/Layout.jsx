948a9905a172d7ac536fd0cf03d0c18fcb3c5923
FerchumanX86 <122957990+FerchumanX86@users.noreply.github.com>
2023-07-13 17:30:18 -0300

    inteno 2

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./NavBar/NavBar"
-import Footer from "./footer/Footer"
+import Footer from "./Footer/Footer"
 
 const Layout = () => {
   return (
