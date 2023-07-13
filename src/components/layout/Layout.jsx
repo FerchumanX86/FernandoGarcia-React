4a076d3b7baaf18fe5b64e264e18bf415f7541a1
FerchumanX86 <122957990+FerchumanX86@users.noreply.github.com>
2023-07-13 17:23:27 -0300

    arreglo de herror

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom"
-import Navbar from "./navbar/Navbar"
+import NavBar from "./navbar/Navbar"
 import Footer from "./footer/Footer"
 
 const Layout = () => {
   return (
     <div>
-        <Navbar />
+        <NavBar />
         <div style={{minHeight: "100vh"}}>
 
         <Outlet />
