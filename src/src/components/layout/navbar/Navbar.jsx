e3eb25eae2881ed94c5158bb6eb782315e103ed8
FerchumanX86 <122957990+FerchumanX86@users.noreply.github.com>
2023-07-14 01:42:01 -0300

    responciv

--- src/src/components/layout/navbar/Navbar.jsx
+++ src/src/components/layout/navbar/Navbar.jsx
@@ -1,7 +1,6 @@
-import { Badge, Button } from "@mui/material";
+import { AppBar, Toolbar, Badge, Button, IconButton } from "@mui/material";
 import { BsFillCartCheckFill } from "react-icons/bs";
-import styles from "./Navbar.module.css";
-
+import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
 import { menuNavigate } from "../../../routes/menuNavigate";
 import { useContext } from "react";
@@ -12,15 +11,17 @@ const Navbar = () => {
   let totalItems = getTotalItems();
 
   return (
-    <>
-      <div className={styles.containerNavbar}>
+    <AppBar position="static">
+      <Toolbar>
         <Link to="/">
           <img
             src="https://res.cloudinary.com/dyt2e5vc9/image/upload/v1685443169/MERCADO%20PAVO/mercado_pavo_hmqwrz.png"
             height={80}
+            alt="Mercado Pavo Logo"
           />
         </Link>
-        <ul className={styles.categories}>
+        <div style={{ flexGrow: 1 }} />
+        <ul style={{ display: "flex", alignItems: "center" }}>
           {menuNavigate.map(({ id, path, title }) => (
             <Button
               key={id}
@@ -28,24 +29,82 @@ const Navbar = () => {
               to={path}
               variant="contained"
               color="primary"
-              
+              style={{ margin: "0 8px" }}
             >
               {title}
             </Button>
           ))}
+          <Link to="/carrito" style={{ marginLeft: "16px" }}>
+            <Badge badgeContent={totalItems} showZero color="primary">
+              <BsFillCartCheckFill size="30px" />
+            </Badge>
+          </Link>
         </ul>
-
-        <Link to="/carrito">
-          <Badge badgeContent={totalItems} showZero color="primary">
-            <BsFillCartCheckFill size="30px" />
-          </Badge>
-        </Link>
-      </div>
-    </>
+        <IconButton
+          edge="end"
+          color="inherit"
+          aria-label="menu"
+          style={{ marginLeft: "16px" }}
+        >
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 };
 
 export default Navbar;
+
+
+
+// import { Badge, Button } from "@mui/material";
+// import { BsFillCartCheckFill } from "react-icons/bs";
+// import styles from "./Navbar.module.css";
+
+// import { Link } from "react-router-dom";
+// import { menuNavigate } from "../../../routes/menuNavigate";
+// import { useContext } from "react";
+// import { CartContext } from "../../../context/CartContext";
+
+// const Navbar = () => {
+//   const { getTotalItems } = useContext(CartContext);
+//   let totalItems = getTotalItems();
+
+//   return (
+//     <>
+//       <div className={styles.containerNavbar}>
+//         <Link to="/">
+//           <img
+//             src="https://res.cloudinary.com/dyt2e5vc9/image/upload/v1685443169/MERCADO%20PAVO/mercado_pavo_hmqwrz.png"
+//             height={80}
+//           />
+//         </Link>
+//         <ul className={styles.categories}>
+//           {menuNavigate.map(({ id, path, title }) => (
+//             <Button
+//               key={id}
+//               component={Link}
+//               to={path}
+//               variant="contained"
+//               color="primary"
+              
+//             >
+//               {title}
+//             </Button>
+//           ))}
+//         </ul>
+
+//         <Link to="/carrito">
+//           <Badge badgeContent={totalItems} showZero color="primary">
+//             <BsFillCartCheckFill size="30px" />
+//           </Badge>
+//         </Link>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Navbar;
 
 
 
