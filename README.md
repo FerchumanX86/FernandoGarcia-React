9a3748930e448a6f613db5761a54868ecdf7f3a0
FerchumanX86 <122957990+FerchumanX86@users.noreply.github.com>
2023-07-14 01:50:46 -0300

    redmi2

--- README.md
+++ README.md
@@ -9,4 +9,24 @@
 ### enlase de VERCEL
 https://fernando-garcia-mercadopavo.vercel.app/
 
+## GIF
+
 ![](https://res.cloudinary.com/dyt2e5vc9/image/upload/v1689308998/MERCADO%20PAVO/ikrzcqxjwbhzikwf7liv.gif)
+
+
+https://res.cloudinary.com/dyt2e5vc9/image/upload/v1689308998/MERCADO%20PAVO/ikrzcqxjwbhzikwf7liv.gif
+
+
+#### VARIABLES DE ENTORNO 
+
+```
+VITE_API_KEY=AIzaSyC9gaCv2aa83SViJMWNwu6eDQcB4RvZ5uU
+VITE_AUTH_DOMAIN=mercado-pavo.firebaseapp.com
+VITE_PROJECT_ID=mercado-pavo
+VITE_STORAGE_BUCKET=mercado-pavo.appspot.com
+VITE_MESSAGING_SENDER_ID=251884467547
+VITE_APP_ID=1:251884467547:web:2838e44cb7bf818678967e
+
+```
+
+##### Fernado Daniel Garcia: comicion 43240
--- src/src/components/layout/navbar/Navbar.jsx
+++ src/src/components/layout/navbar/Navbar.jsx
@@ -57,54 +57,5 @@ export default Navbar;
 
 
 
-// import { Badge, Button } from "@mui/material";
-// import { BsFillCartCheckFill } from "react-icons/bs";
-// import styles from "./Navbar.module.css";
-
-// import { Link } from "react-router-dom";
-// import { menuNavigate } from "../../../routes/menuNavigate";
-// import { useContext } from "react";
-// import { CartContext } from "../../../context/CartContext";
-
-// const Navbar = () => {
-//   const { getTotalItems } = useContext(CartContext);
-//   let totalItems = getTotalItems();
-
-//   return (
-//     <>
-//       <div className={styles.containerNavbar}>
-//         <Link to="/">
-//           <img
-//             src="https://res.cloudinary.com/dyt2e5vc9/image/upload/v1685443169/MERCADO%20PAVO/mercado_pavo_hmqwrz.png"
-//             height={80}
-//           />
-//         </Link>
-//         <ul className={styles.categories}>
-//           {menuNavigate.map(({ id, path, title }) => (
-//             <Button
-//               key={id}
-//               component={Link}
-//               to={path}
-//               variant="contained"
-//               color="primary"
-              
-//             >
-//               {title}
-//             </Button>
-//           ))}
-//         </ul>
-
-//         <Link to="/carrito">
-//           <Badge badgeContent={totalItems} showZero color="primary">
-//             <BsFillCartCheckFill size="30px" />
-//           </Badge>
-//         </Link>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Navbar;
-
 
 
